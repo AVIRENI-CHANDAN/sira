c2eb6cf2eff02f4c0a4c26b400fdf602cfc36804
AVIRENI-CHANDAN <avireni.chandanchandu43657@gmail.com>
2024-07-13 08:32:35 +0530

    Application navbar and page created.

---
+++ frontend/web/src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './NavigationBar.module.scss';
+
+const NavigationBar = () => (
+  <div className={styles.NavigationBar}>
+    NavigationBar Component
+  </div>
+);
+
+NavigationBar.propTypes = {};
+
+NavigationBar.defaultProps = {};
+
+export default NavigationBar;
