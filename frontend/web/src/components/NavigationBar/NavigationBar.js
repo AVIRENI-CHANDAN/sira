f9660c1ecf275447c21b2190e1843d2d11fbe69f
AVIRENI-CHANDAN <avireni.chandanchandu43657@gmail.com>
2024-07-13 08:54:14 +0530

    The logo added to the navigation bar

--- frontend/web/src/components/NavigationBar/NavigationBar.js
+++ frontend/web/src/components/NavigationBar/NavigationBar.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import logo_64 from '../../images/logo64.png';
 import styles from './NavigationBar.module.scss';
 
-const NavigationBar = () => (
-  <div className={styles.NavigationBar}>
-    NavigationBar Component
-  </div>
-);
+class NavigationBar extends React.Component {
+  render() {
+    return (
+      <header className={styles.NavigationBar}>
+        <div className={styles.LogoContainer}>
+          <div className={styles.LogoWrapper}>
+            <img src={logo_64} alt="64Logo" />
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
 
 NavigationBar.propTypes = {};
 
