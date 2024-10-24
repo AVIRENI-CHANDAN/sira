ac9ffba1451b4fcad566a124cd18b715841e355d
Avireni Chandan <avireni.chandanchandu43657@gmail.com>
2024-10-23 23:46:50 -0400

    Suggested changes update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,4 +11,4 @@ def application_home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=os.getenv("FLASK_DEBUG", "False").lower() == "true")
